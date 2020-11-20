4f8ff13e0f2b597b24536aa72ec3af312326c231
Artur Dybysławski <adybyslawski@hotmail.com>
2020-11-20 22:07:13 +0100

    REV_22.15

---
+++ ClientApp/src/app/messages/message.model.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Message = /** @class */ (function () {
+    function Message(text, error) {
+        if (error === void 0) { error = false; }
+        this.text = text;
+        this.error = error;
+    }
+    return Message;
+}());
+exports.Message = Message;
+//# sourceMappingURL=message.model.js.map
