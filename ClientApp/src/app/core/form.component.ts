e421e555542eff5b8879e9b3b687ecf5a2b0ad65
Artur Dybysławski <adybyslawski@hotmail.com>
2020-11-21 10:56:58 +0100

    REV_STANDARD

--- ClientApp/src/app/core/form.component.ts
+++ ClientApp/src/app/core/form.component.ts
@@ -1,8 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, Inject } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Product } from "../model/product.model";
 import { Model } from "../model/repository.model";
-import { MODES, SharedState } from "./sharedState.model";
+import { MODES, SharedState, SHARED_STATE } from "./sharedState.model";
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: "paForm",
@@ -12,14 +13,18 @@ import { MODES, SharedState } from "./sharedState.model";
 })
 export class FormComponent {
   product: Product = new Product();
-
-  constructor(private model: Model, private state: SharedState) {
-
+  
+  constructor(private model: Model, @Inject(SHARED_STATE) private stateEvents: Observable<SharedState>) {
+    stateEvents.subscribe((update) => {
+      this.product = new Product();
+      if (update.id != undefined) {
+        Object.assign(this.product, this.model.getProduct(update.id));
+      }
+      this.editing = update.mode == MODES.EDIT;
+    });
   }
 
-  get editing(): boolean {
-    return this.state.mode == MODES.EDIT;
-  }
+  editing: boolean;
 
   submitForm(form: NgForm) {
     if (form.valid) {
--- ClientApp/src/app/core/sharedState.model.js
+++ ClientApp/src/app/core/sharedState.model.js
@@ -1,15 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
 var MODES;
 (function (MODES) {
     MODES[MODES["CREATE"] = 0] = "CREATE";
     MODES[MODES["EDIT"] = 1] = "EDIT";
 })(MODES = exports.MODES || (exports.MODES = {}));
 var SharedState = /** @class */ (function () {
-    function SharedState() {
-        this.mode = MODES.EDIT;
+    function SharedState(mode, id) {
+        this.mode = mode;
+        this.id = id;
     }
     return SharedState;
 }());
 exports.SharedState = SharedState;
+exports.SHARED_STATE = new core_1.InjectionToken("shared_state");
 //# sourceMappingURL=sharedState.model.js.map
