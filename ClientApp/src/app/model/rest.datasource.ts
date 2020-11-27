679a2a202dd22ab761d4c15809515ba4e634bf4d
Artur Dybysławski <adybyslawski@hotmail.com>
2020-11-27 22:32:20 +0100

    REV_24.13

--- ClientApp/src/app/model/rest.datasource.ts
+++ ClientApp/src/app/model/rest.datasource.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject, InjectionToken } from "@angular/core";
-import { Http, Request, RequestMethod, Jsonp } from "@angular/http";
+import { Http, Request, RequestMethod, Headers} from "@angular/http";
 import { Observable } from "rxjs/Observable";
 import { Product } from "./product.model";
 import "rxjs/operator/map";
@@ -8,11 +8,11 @@ export const REST_URL = new InjectionToken("rest_url");
 
 @Injectable()
 export class RestDataSource {
-  constructor(private http: Http, private jsonp: Jsonp, @Inject(REST_URL) private url: string) { }
+  constructor(private http: Http, @Inject(REST_URL) private url: string) { }
 
   //-- SPRAWDŹ W TYPE SCRIPT UNIE TYPÓW CZYLI ZWRACANIE RÓZNYCH TYPÓW
   getData(): Observable<Product[]> {
-    return this.jsonp.get(this.url + "?callback=JSONP_CALLBACK").map(response => response.json());
+    return  <Observable<Product[]>>  this.sendRequest(RequestMethod.Get, this.url);
   }
 
   saveProduct(product: Product): Observable<Product> {
@@ -31,7 +31,12 @@ export class RestDataSource {
     return <Observable<Product>> this.http.request(new Request({
       method: verb,
       url: url,
-      body: body
+      body: body,
+      headers: new Headers(
+        {
+          "Access-Key": "<sekret>",
+          "Application-Name": "<exampleApp>"
+        })
     })).map(response => response.json());
   }
 
