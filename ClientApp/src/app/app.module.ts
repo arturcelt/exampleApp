181939099716dae6aa8cbfc768ef14f3f752c89c
Artur Dybysławski <adybyslawski@hotmail.com>
2020-11-30 18:56:14 +0100

    REV_25.8

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { TableComponent } from "./core/table.component";
 import { FormComponent } from "./core/form.component";
 import { MessageModule } from "./messages/message.module";
 import { MessageComponent } from "./messages/message.component";
+import { routing } from "./app.routing";
+import { AppComponent } from "./app.component";
 
 import { registerLocaleData } from '@angular/common';
 import localePl from '@angular/common/locales/pl';
@@ -15,27 +17,11 @@ registerLocaleData(localePl);
 
 @NgModule({
   declarations: [
-    
+    AppComponent
   ],
-  imports: [BrowserModule, ModelModule, CoreModule, MessageModule],
+  imports: [BrowserModule, ModelModule, CoreModule, MessageModule, routing],
   providers: [{provide: LOCALE_ID, useValue: "pl-PL"}],
-  bootstrap: [TableComponent, FormComponent, MessageComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
 
-
-
-
-
-
-//@NgModule({
-//  declarations: [
-
-//  ],
-//  imports: [
-//    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' })
-//  ],
-//  providers: [],
-//  bootstrap: []
-//})
-//export class AppModule { }
