bebf2f9d499bf73698d8cb0e17f3343c9caf229c
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-01 22:14:00 +0100

    REV_26.10

--- ClientApp/src/app/app.routing.js
+++ ClientApp/src/app/app.routing.js
@@ -7,6 +7,9 @@ var notFoundComponent_1 = require("./core/notFoundComponent");
 var routes = [
     { path: "form/:mode/:id", component: form_component_1.FormComponent },
     { path: "form/:mode", component: form_component_1.FormComponent },
+    { path: "nie", redirectTo: "/form/create", pathMatch: "prefix" },
+    { path: "table", component: table_component_1.TableComponent },
+    { path: "", redirectTo: "/table", pathMatch: "full" },
     { path: "", component: table_component_1.TableComponent },
     { path: "**", component: notFoundComponent_1.NotFoundComponent }
 ];
--- ClientApp/src/app/app.routing.ts
+++ ClientApp/src/app/app.routing.ts
@@ -6,6 +6,9 @@ import { NotFoundComponent } from "./core/notFoundComponent";
 const routes: Routes = [
   { path: "form/:mode/:id", component: FormComponent },
   { path: "form/:mode", component: FormComponent },
+  { path: "nie", redirectTo: "/form/create", pathMatch: "prefix" },
+  { path: "table", component: TableComponent },
+  { path: "", redirectTo: "/table", pathMatch: "full"},
   { path: "", component: TableComponent },
   { path: "**", component: NotFoundComponent}
 ]
