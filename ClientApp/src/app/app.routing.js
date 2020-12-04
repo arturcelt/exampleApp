d68e485ab175cf0fad58bee3df3096eb67f12350
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-04 19:30:53 +0100

    REV_26.17

--- ClientApp/src/app/app.routing.js
+++ ClientApp/src/app/app.routing.js
@@ -4,10 +4,20 @@ var router_1 = require("@angular/router");
 var table_component_1 = require("./core/table.component");
 var form_component_1 = require("./core/form.component");
 var notFoundComponent_1 = require("./core/notFoundComponent");
+var productCount_component_1 = require("./core/productCount.component");
+var categoryCount_component_1 = require("./core/categoryCount.component");
 var routes = [
     { path: "form/:mode/:id", component: form_component_1.FormComponent },
     { path: "form/:mode", component: form_component_1.FormComponent },
     { path: "nie", redirectTo: "/form/create", pathMatch: "prefix" },
+    {
+        path: "table",
+        component: table_component_1.TableComponent,
+        children: [
+            { path: "products", component: productCount_component_1.ProductCountComponent },
+            { path: "categories", component: categoryCount_component_1.CategoryCountComponent }
+        ]
+    },
     { path: "table/:category", component: table_component_1.TableComponent },
     { path: "table", component: table_component_1.TableComponent },
     { path: "", redirectTo: "/table", pathMatch: "full" },
