c6ceaea3f30e2ad00a52a412b30cd94f1f5361d1
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-04 22:55:59 +0100

    REV_27.6

--- ClientApp/src/app/model/model.module.ts
+++ ClientApp/src/app/model/model.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { Model } from "./repository.model";
 import { HttpModule, JsonpModule } from "@angular/http";
 import { RestDataSource, REST_URL } from "./rest.datasource";
+import { ModelResolver } from "./model.resolver";
 
 @NgModule({
   imports: [HttpModule, JsonpModule],
-  providers: [Model, RestDataSource,
+  providers: [Model, RestDataSource, ModelResolver,
     { provide: REST_URL, useValue: `http://${location.hostname}:19626/api/Product` }]
 })
 export class ModelModule {
--- ClientApp/src/app/model/model.resolver.ts
+++ ClientApp/src/app/model/model.resolver.ts
@@ -4,12 +4,18 @@ import { Observable } from "rxjs/Observable";
 import { Model } from "./repository.model";
 import { RestDataSource } from "./rest.datasource";
 import { Product } from "./product.model";
+import { MessageService } from "../messages/message.service";
+import { Message } from "../messages/message.model";
 
 @Injectable()
 export class ModelResolver {
-  constructor(private model: Model, private dataSource: RestDataSource) { }
+  constructor(private model: Model, private dataSource: RestDataSource, private messages: MessageService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product[]> {
-    return this.model.getProducts().length == 0 ? this.dataSource.getData() : null;
+    if (this.model.getProducts().length == 0) {
+      this.messages.reportMessage(new Message("Wczytywanie danych..."));
+      return this.dataSource.getData();
+    }
+    
   }
 }
