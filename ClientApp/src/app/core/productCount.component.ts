d720354736c8529dd16082fb82a8bf2aff85f3c2
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-04 21:22:05 +0100

    REV_26.21

--- ClientApp/src/app/core/productCount.component.ts
+++ ClientApp/src/app/core/productCount.component.ts
@@ -1,5 +1,6 @@
 import { Component, KeyValueDiffer, KeyValueDiffers, ChangeDetectorRef } from "@angular/core";
 import { Model } from "../model/repository.model";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "paProductCount",
@@ -8,8 +9,16 @@ import { Model } from "../model/repository.model";
 export class ProductCountComponent {
   private differ: KeyValueDiffer<any, any>;
   count: number = 0;
+  private category: string;
 
-  constructor(private model: Model, private keyValueDifers: KeyValueDiffers, private changeDetector: ChangeDetectorRef) { }
+  constructor(private model: Model, private keyValueDifers: KeyValueDiffers, private changeDetector: ChangeDetectorRef, activeRoute: ActivatedRoute) {
+    activeRoute.pathFromRoot.forEach(route => route.params.subscribe(params => {
+      if (params["category"] != null) {
+        this.category = params["category"];
+        this.updateCount();
+      }
+    }))
+  }
 
   ngOnInit() {
     this.differ = this.keyValueDifers
@@ -24,7 +33,9 @@ export class ProductCountComponent {
   }
 
   private updateCount() {
-    this.count = this.model.getProducts().length;
+    this.count = this.model.getProducts()
+      .filter(p => this.category == null || p.category == this.category)
+      .length;
   }
 
 }
