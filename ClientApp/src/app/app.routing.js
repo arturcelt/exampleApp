21a4cfacb711ffd2b21bbd3c78a4833343bfd9f7
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-04 19:58:19 +0100

    REV_26.19

--- ClientApp/src/app/app.routing.js
+++ ClientApp/src/app/app.routing.js
@@ -6,22 +6,18 @@ var form_component_1 = require("./core/form.component");
 var notFoundComponent_1 = require("./core/notFoundComponent");
 var productCount_component_1 = require("./core/productCount.component");
 var categoryCount_component_1 = require("./core/categoryCount.component");
+var childRoutes = [
+    { path: "products", component: productCount_component_1.ProductCountComponent },
+    { path: "categories", component: categoryCount_component_1.CategoryCountComponent },
+    { path: "", component: productCount_component_1.ProductCountComponent }
+];
 var routes = [
     { path: "form/:mode/:id", component: form_component_1.FormComponent },
     { path: "form/:mode", component: form_component_1.FormComponent },
     { path: "nie", redirectTo: "/form/create", pathMatch: "prefix" },
-    {
-        path: "table",
-        component: table_component_1.TableComponent,
-        children: [
-            { path: "products", component: productCount_component_1.ProductCountComponent },
-            { path: "categories", component: categoryCount_component_1.CategoryCountComponent }
-        ]
-    },
-    { path: "table/:category", component: table_component_1.TableComponent },
-    { path: "table", component: table_component_1.TableComponent },
+    { path: "table", component: table_component_1.TableComponent, children: childRoutes },
+    { path: "table/:category", component: table_component_1.TableComponent, children: childRoutes },
     { path: "", redirectTo: "/table", pathMatch: "full" },
-    { path: "", component: table_component_1.TableComponent },
     { path: "**", component: notFoundComponent_1.NotFoundComponent }
 ];
 exports.routing = router_1.RouterModule.forRoot(routes);
--- ClientApp/src/app/app.routing.ts
+++ ClientApp/src/app/app.routing.ts
@@ -5,24 +5,20 @@ import { NotFoundComponent } from "./core/notFoundComponent";
 import { ProductCountComponent } from "./core/productCount.component";
 import { CategoryCountComponent } from "./core/categoryCount.component";
 
+const childRoutes: Routes = [
+  { path: "products", component: ProductCountComponent },
+  { path: "categories", component: CategoryCountComponent },
+  { path: "", component: ProductCountComponent }
+]
+
 
 const routes: Routes = [
   { path: "form/:mode/:id", component: FormComponent },
   { path: "form/:mode", component: FormComponent },
   { path: "nie", redirectTo: "/form/create", pathMatch: "prefix" },
-  {
-    path: "table",
-    component: TableComponent,
-    children: [
-      { path: "products", component: ProductCountComponent },
-      { path: "categories", component: CategoryCountComponent }
-    ]
-  },
-  { path: "table/:category", component: TableComponent },
-
-  { path: "table", component: TableComponent },
+  { path: "table", component: TableComponent, children: childRoutes },
+  { path: "table/:category", component: TableComponent, children: childRoutes},
   { path: "", redirectTo: "/table", pathMatch: "full" },
-  { path: "", component: TableComponent },
   { path: "**", component: NotFoundComponent}
 ]
 
