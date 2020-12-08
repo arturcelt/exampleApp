b55e6ee2726ab32310c1dd4e322835da191016a0
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-08 21:21:35 +0100

    REV_27.20

--- ClientApp/src/app/core/core.module.ts
+++ ClientApp/src/app/core/core.module.ts
@@ -20,7 +20,7 @@ import { UnsavedGuard } from "./unsaved.guard";
 @NgModule({
   imports: [BrowserModule, FormsModule, ModelModule, MessageModule, RouterModule],
   declarations: [TableComponent, FormComponent, StatePipe, ProductCountComponent, CategoryCountComponent, NotFoundComponent],
-  providers: [UnsavedGuard]
+  providers: [UnsavedGuard],
   exports: [ModelModule, TableComponent, FormComponent]
 })
 export class CoreModule { }
