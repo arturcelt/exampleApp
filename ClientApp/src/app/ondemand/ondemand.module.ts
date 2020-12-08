8c30ca03ed8c21662a19d6387b2ab3e629ebfc8b
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-08 21:35:12 +0100

    REV_27.3

--- ClientApp/src/app/ondemand/ondemand.module.ts
+++ ClientApp/src/app/ondemand/ondemand.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { OndemandComponent } from "./ondemand.component";
 import { NgModel } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+
+let routing = RouterModule.forChild([{ path: "", component: OndemandComponent }]);
+
+
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, routing],
   declarations: [OndemandComponent],
   exports: [OndemandComponent]
 })
