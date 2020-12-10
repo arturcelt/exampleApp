3c50e06cc289d1668020c9a4c16d9c42c423800b
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-10 19:34:52 +0100

    REV_27.30

---
+++ ClientApp/src/app/ondemand/second.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "second",
+  template: `<div class="bg-info p-a-1">Drugi komponent.<div>`
+})
+export class SecondComponent { }
+
