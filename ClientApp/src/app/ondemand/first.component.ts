9445902f60454fb756619440111a6178e7cf5a73
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-10 18:56:14 +0100

    REV_27.29

---
+++ ClientApp/src/app/ondemand/first.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "first",
+  template: `<div class="bg-primary p-a-1">Pierwszy komponent.</div>`
+})
+export class FirstComponent {
+
+}
