7afad776ca8abc8308f1072003d174aca870ecf8
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-10 18:38:10 +0100

    REV_27.26

---
+++ ClientApp/src/app/load.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { Route, Router } from "@angular/router";
+import { MessageService } from "./messages/message.service";
+import { Message } from "./messages/message.model";
+
+@Injectable()
+export class LoadGuard {
+  private loaded: boolean = false;
+  constructor(private messages: MessageService, private router: Router) { }
+
+  canLoad(route: Router): Promise<boolean> | boolean {
+    return this.loaded || new Promise<boolean>((resolve, reject) => {
+      let responses: [[string, (string) => void], [string, (string) => void]] =
+        [
+          ["Tak", () => {
+            this.loaded = true;
+            resolve(true);
+          }],
+          [
+            "Nie", () => {
+              this.router.navigateByUrl(this.router.url);
+              resolve(false);
+            }
+          ]
+        ];
+    });
+  }
+
+}
