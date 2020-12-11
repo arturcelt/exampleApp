9dd79c8c7425ef88737edc2e7b5520c9d2cbc697
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-11 21:58:02 +0100

    REV_28.7

--- ClientApp/src/app/core/table.component.ts
+++ ClientApp/src/app/core/table.component.ts
@@ -2,12 +2,14 @@ import { Component, Inject } from "@angular/core";
 import { Product } from "../model/product.model";
 import { Model } from "../model/repository.model";
 import { ActivatedRoute } from "@angular/router";
+import { HighlightTrigger } from "./table.animations";
 
 @Component(
   {
     selector: "paTable",
     moduleId: module.id,
-    templateUrl: "table.component.html"
+    templateUrl: "table.component.html",
+    animations: [HighlightTrigger]
   })
 export class TableComponent {
   category: string = null;
@@ -34,6 +36,11 @@ export class TableComponent {
 
   deleteProduct(key: number) {
     this.model.deleteProduct(key);
+  }
+
+  highlightCategory: string = "";
+  getRowState(category: string): string {
+    return this.highlightCategory == "" ? "" : this.highlightCategory == category ? "selected" : "notselected";
   }
 
   }
