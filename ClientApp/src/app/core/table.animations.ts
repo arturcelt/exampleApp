5ec7d63c529a0463a8a5c108f322e6929be1ceee
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-12 12:31:35 +0100

    REV_28.16

--- ClientApp/src/app/core/table.animations.ts
+++ ClientApp/src/app/core/table.animations.ts
@@ -1,28 +1,23 @@
-import { trigger, style, state, transition, animate } from "@angular/core";
+import { trigger, style, state, transition, animate, group } from "@angular/core";
 
+const commonStyles = {
+  border: "black solid 4px",
+  color: "white"
+};
 
 export const HighlightTrigger = trigger("rowHighlight", [
-  state("selected", style({
+  state("selected", style([commonStyles,
+    {
     backgroundColor: "lightgreen",
     fontSize: "20px"
-  })),
-  state("notselected", style({
+  }])),
+  state("notselected", style([commonStyles, {
     backgroundColor: "lightsalmon",
-    fontSize: "12px"
-  })),
+    fontSize: "12px",
+    color: "black"
+  }])),
   state("void", style({ opacity: 0 })),
   transition("* => notselected", animate("200ms")),
-  transition("* => selected",
-    [
-      animate("400ms 200ms ease-in", style({
-        backgroundColor: "lightblue",
-        fontSize: "25px"
-      })),
-      animate("250ms", style({
-        backgroundColor: "lightcoral",
-        fontSize: "30px"
-      })),
-      animate("200ms")
-    ]
-  )
+  transition("* => selected", animate("400ms 200ms ease-in")),
+  transition("void => *", animate("500ms"))
 ]);
