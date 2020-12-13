44e8a411957ca746dd902b4e086ccd09969c1919
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-13 12:21:36 +0100

    REV_28.19

--- ClientApp/src/app/core/table.animations.js
+++ ClientApp/src/app/core/table.animations.js
@@ -1,22 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var animationUtils_1 = require("../animationUtils");
 var commonStyles = {
     border: "black solid 4px",
     color: "white"
 };
 exports.HighlightTrigger = core_1.trigger("rowHighlight", [
-    core_1.state("selected", core_1.style([commonStyles,
-        {
-            backgroundColor: "lightgreen",
-            fontSize: "20px"
-        }])),
-    core_1.state("notselected", core_1.style([commonStyles, {
-            backgroundColor: "lightsalmon",
-            fontSize: "12px",
-            color: "black"
-        }])),
-    core_1.state("void", core_1.style({ opacity: 0 })),
+    core_1.state("selected", core_1.style(animationUtils_1.getStylesFromClasses("bg-success", "h2"))),
+    core_1.state("notselected", core_1.style(animationUtils_1.getStylesFromClasses("bg-info"))),
+    core_1.state("void", core_1.style({ transform: "translateX(-50%)" })),
     core_1.transition("* => notselected", core_1.animate("200ms")),
     core_1.transition("* => selected", core_1.animate("400ms 200ms ease-in")),
     core_1.transition("void => *", core_1.animate("500ms"))
--- ClientApp/src/app/core/table.animations.ts
+++ ClientApp/src/app/core/table.animations.ts
@@ -1,4 +1,5 @@
 import { trigger, style, state, transition, animate, group } from "@angular/core";
+import { getStylesFromClasses } from "../animationUtils";
 
 const commonStyles = {
   border: "black solid 4px",
@@ -6,17 +7,9 @@ const commonStyles = {
 };
 
 export const HighlightTrigger = trigger("rowHighlight", [
-  state("selected", style([commonStyles,
-    {
-    backgroundColor: "lightgreen",
-    fontSize: "20px"
-  }])),
-  state("notselected", style([commonStyles, {
-    backgroundColor: "lightsalmon",
-    fontSize: "12px",
-    color: "black"
-  }])),
-  state("void", style({ opacity: 0 })),
+  state("selected", style(getStylesFromClasses("bg-success", "h2"))),
+  state("notselected", style(getStylesFromClasses("bg-info"))),
+  state("void", style({ transform: "translateX(-50%)" })),
   transition("* => notselected", animate("200ms")),
   transition("* => selected", animate("400ms 200ms ease-in")),
   transition("void => *", animate("500ms"))
