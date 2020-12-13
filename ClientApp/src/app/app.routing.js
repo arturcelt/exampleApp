23859d92a07d0ebfab9627f2568404cab9151105
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-13 14:38:39 +0100

    REV_29.1

--- ClientApp/src/app/app.routing.js
+++ ClientApp/src/app/app.routing.js
@@ -1,18 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var router_1 = require("@angular/router");
-var table_component_1 = require("./core/table.component");
-var form_component_1 = require("./core/form.component");
-var notFoundComponent_1 = require("./core/notFoundComponent");
-var model_resolver_1 = require("./model/model.resolver");
-var unsaved_guard_1 = require("./core/unsaved.guard");
 var routes = [
-    { path: "form/:mode/:id", component: form_component_1.FormComponent, resolve: { model: model_resolver_1.ModelResolver }, canDeactivate: [unsaved_guard_1.UnsavedGuard] },
-    { path: "form/:mode", component: form_component_1.FormComponent, resolve: { model: model_resolver_1.ModelResolver }, },
-    { path: "table", component: table_component_1.TableComponent },
-    { path: "table/:category", component: table_component_1.TableComponent },
-    { path: "", redirectTo: "/table", pathMatch: "full" },
-    { path: "**", component: notFoundComponent_1.NotFoundComponent }
+    { path: "ondemand", loadChildren: "app/ondemand/ondemand.module#OndemandModule" },
+    { path: "", redirectTo: "/ondemand", pathMatch: "full" }
 ];
 exports.routing = router_1.RouterModule.forRoot(routes);
 //# sourceMappingURL=app.routing.js.map
