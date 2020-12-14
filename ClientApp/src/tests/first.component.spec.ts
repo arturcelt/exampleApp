f1f1412611f36464df8f2568ba0fcfe9d7be1fb6
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-14 23:19:20 +0100

    REV_29.7

---
+++ ClientApp/src/tests/first.component.spec.ts
@@ -0,0 +1,10 @@
+import { TestBed } from "@angular/core/testing";
+import { FirstComponent } from "../app/ondemand/first.component";
+
+describe("FirstComponent", () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [FirstComponent]
+    });
+  });
+});
