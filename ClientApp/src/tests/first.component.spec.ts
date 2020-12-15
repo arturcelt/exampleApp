b60c00b83a5dc2ac325b474c4e0ffe591722820b
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-15 23:04:51 +0100

    REV_29.10

--- ClientApp/src/tests/first.component.spec.ts
+++ ClientApp/src/tests/first.component.spec.ts
@@ -1,12 +1,28 @@
 import { TestBed, ComponentFixture } from "@angular/core/testing";
 import { FirstComponent } from "../app/ondemand/first.component";
+import { Product } from "../app/model/product.model";
+import { Model } from "../app/model/repository.model";
 
 describe("FirstComponent", () => {
   let fixture: ComponentFixture<FirstComponent>;
   let component: FirstComponent;
+
+  let mockRepository = {
+    getProducts: function () {
+      return [
+        new Product(1, "test1", "Piłka nożna", 100),
+        new Product(2, "test2", "Szachy", 100),
+        new Product(3, "test3", "Piłka nożna", 100)
+      ]
+    }
+  };
+
+
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FirstComponent]
+      declarations: [FirstComponent],
+      providers: [{ provide: Model, useValue: mockRepository }]
     });
     fixture = TestBed.createComponent(FirstComponent);
     component = fixture.componentInstance;
@@ -14,5 +30,14 @@ describe("FirstComponent", () => {
   });
   it("Sprawdzenie czy komponent jest zdefiniowany", () => {
     expect(component).toBeDefined()
+  })
+
+  it("Filtrowanie kategorii", () => {
+    component.category = "Szachy";
+    expect(component.getProducts().length).toBe(1);
+    component.category = "Piłka nożna";
+    expect(component.getProducts().length).toBe(2);
+    component.category = "Bieganie";
+    expect(component.getProducts().length).toBe(0);
   })
 });
