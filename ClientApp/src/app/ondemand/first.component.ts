01f5fc3e7ba81263ef7b1a61a43fbf4a2bb70a2e
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-15 22:31:57 +0100

    REV_29.9

--- ClientApp/src/app/ondemand/first.component.ts
+++ ClientApp/src/app/ondemand/first.component.ts
@@ -1,9 +1,23 @@
 import { Component } from "@angular/core";
+import { Product } from "../model/product.model";
+import { Model } from "../model/repository.model";
 
 @Component({
   selector: "first",
-  template: `<div class="bg-primary p-a-1">Pierwszy komponent.</div>`
+  template: `<div class="bg-primary p-a-1">
+Mamy
+<span class="strong">{{getProducts().length}}</span>
+Produktów.
+</div>
+`
 })
 export class FirstComponent {
+  constructor(private repository: Model) { }
+
+  category: string = "Piłka nożna";
+
+  getProducts(): Product[] {
+    return this.repository.getProducts().filter(p => p.category == this.category);
+  }
 
 }
