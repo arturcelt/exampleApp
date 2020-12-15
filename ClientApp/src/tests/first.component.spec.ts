2311d685f7739e55c0c9da0cce5ba43f7f1c4b33
Artur Dybysławski <adybyslawski@hotmail.com>
2020-12-15 22:25:13 +0100

    REV_29.8

--- ClientApp/src/tests/first.component.spec.ts
+++ ClientApp/src/tests/first.component.spec.ts
@@ -1,10 +1,18 @@
-import { TestBed } from "@angular/core/testing";
+import { TestBed, ComponentFixture } from "@angular/core/testing";
 import { FirstComponent } from "../app/ondemand/first.component";
 
 describe("FirstComponent", () => {
+  let fixture: ComponentFixture<FirstComponent>;
+  let component: FirstComponent;
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [FirstComponent]
     });
+    fixture = TestBed.createComponent(FirstComponent);
+    component = fixture.componentInstance;
+
   });
+  it("Sprawdzenie czy komponent jest zdefiniowany", () => {
+    expect(component).toBeDefined()
+  })
 });
--- ClientApp/src/tests/frameworkTest.spec.ts
+++ ClientApp/src/tests/frameworkTest.spec.ts
@@ -1,5 +1,5 @@
 describe("Środowisko testowe Jasmine", () => {
-  it("Test wartości liczbowej", () => expect(9).toBeGreaterThan(10));
+  it("Test wartości liczbowej", () => expect(11).toBeGreaterThan(10));
   it("Test wartości tekstowej", () => expect("Gliwice").toMatch("^Gli"));
 
 });
